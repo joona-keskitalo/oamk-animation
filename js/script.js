cdec5955f68c01b2783f4e20b40d46e7978ebfab
Joona <joona@Joonas-iMac.local>
2017-12-17 21:10:19 +0200

    Movie booking system for the main page
    Also removed unnecessary files

--- js/script.js
+++ js/script.js
@@ -1,44 +1,14 @@
+	function showOverlay() {
+		document.getElementById("bookingOverlay").style.display = "block";
+	}
 
-
-
-
-// var slideIndex = 1;
-// showDivs(slideIndex);
-
-// function plusDivs(n) {
-//   showDivs(slideIndex += n);
-// }
-
-// function currentDiv(n) {
-//   showDivs(slideIndex = n);
-// }
-
-// function showDivs(n) {
-//   var i;
-//   var x = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("demo");
-//   if (n > x.length) {slideIndex = 1}    
-//   if (n < 1) {slideIndex = x.length}
-//   for (i = 0; i < x.length; i++) {
-//      x[i].style.display = "none";  
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//      dots[i].className = dots[i].className.replace(" w3-white", "");
-//   }
-//   x[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " w3-white";
-// }
-
-
-
-
-
-
-
+	function hideOverlay() {
+		document.getElementById("bookingOverlay").style.display = "none";
+	}
 
 function posterFade(posterImage){
-	$('#moviePoster').fadeOut(300, function(){
-		$(this).html(posterImage).fadeIn(300);
+	$('#moviePoster').fadeOut(250, function(){
+		$(this).html(posterImage).fadeIn(250);
 	})
 }
 function movieName(movie, studio, released, length, rating, desc){
@@ -49,32 +19,47 @@ function movieName(movie, studio, released, length, rating, desc){
 	$('#movieRating').html(rating);
 	$('#movieDescription').html(desc);
 }
-// function movieReleased(movie){
-// 	$('#movieName').html(movie);
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function() {
 	$('#p1').on('click', function() {
-		movieName("Alien","STUDIO","releaseYEAR", "LENGTH", "RATING", "DESC");
+		movieName("Alien","20th Century Fox","1979", "1h 57 min", "8.5", "Alien is a 1979 science-fiction horror film directed by Ridley Scott, and starring Sigourney Weaver, Tom Skerritt, Veronica Cartwright, Harry Dean Stanton, John Hurt, Ian Holm and Yaphet Kotto.");
 		posterFade("<img src='img/poster_001.jpg'</img>");
 	});
 	$('#p2').on('click', function() {
-		movieName("Aftermath","STUDIO","releaseYEAR", "LENGTH", "RATING", "DESC");
+		movieName("Aftermath","Lionsgate","2017", "92 minutes", "5.7", "Aftermath (originally titled 478) is a 2017 American drama thriller film directed by Elliott Lester and written by Javier Gullón. The film stars Arnold Schwarzenegger, Scoot McNairy, Maggie Grace and Martin Donovan.");
 		posterFade("<img src='img/poster_002.jpg'</img>");
 	});
 	$('#p3').on('click', function() {
-		movieName("Baby Driver","STUDIO","releaseYEAR", "LENGTH", "RATING", "DESC");
+		movieName("Baby Driver","TriStar Pictures & Sony","2017", "1h 53min", "7.8", "Talented getaway driver Baby (Ansel Elgort) relies on the beat of his personal soundtrack to be the best in the game. After meeting the woman (Lily James) of his dreams, he sees a chance to ditch his shady lifestyle and make a clean break. ");
 		posterFade("<img src='img/poster_003.jpg'</img>");
 	});
 	$('#p4').on('click', function() {
-		movieName("Guardians of the Galaxy Vol. 2","STUDIO","releaseYEAR", "LENGTH", "RATING", "DESC");
+		movieName("Guardians of the Galaxy Vol. 2","Marvel Studios","2017", "2h 18min", "7.8", "Peter Quill and his fellow Guardians are hired by a powerful alien race, the Sovereign, to protect their precious batteries from invaders. When it is discovered that Rocket has stolen the items they were sent to guard, the Sovereign dispatch their armada to search for vengeance.");
 		posterFade("<img src='img/poster_004.jpg'</img>");
 	});
 	$('#p5').on('click', function() {
-		movieName("Moonlight","STUDIO","releaseYEAR", "LENGTH", "RATING", "DESC");
+		movieName("Moonlight","A24","2016", "1h 55m", "7.5", "A look at three defining chapters in the life of Chiron, a young black man growing up in Miami. His epic journey to manhood is guided by the kindness, support and love of the community that helps raise him.");
 		posterFade("<img src='img/poster_005.jpg'</img>");
 	});
 	$('#p6').on('click', function() {
-		movieName("All the Money in the World","STUDIO","releaseYEAR", "LENGTH", "RATING", "DESC");
+		movieName("All the Money in the World","STUDIO","2017", "132 min", "NA", "ALL THE MONEY IN THE WORLD follows the kidnapping of 16-year-old John Paul Getty III (Charlie Plummer) and the desperate attempt by his devoted mother Gail (Michelle Williams) to convince his billionaire grandfather (Christopher Plummer) to pay the ransom.");
 		posterFade("<img src='img/poster_006.jpg'</img>");
 	});
 	$('#p7').on('click', function() {
@@ -97,25 +82,96 @@ $(document).ready(function() {
 		movieName("Divergent","STUDIO","releaseYEAR", "LENGTH", "RATING", "DESC");
 		posterFade("<img src='img/poster_011.jpg'</img>");
 	});
-
-
-
-
-
-
-
-
-
-
-
-
-
+	$('#p12').on('click', function() {
+		movieName("Star Wars: The Empire Strikes Back","STUDIO","releaseYEAR", "LENGTH", "RATING", "DESC");
+		posterFade("<img src='img/poster_012.jpg'</img>");
+	});
+	$('#p13').on('click', function() {
+		movieName("Star Wars: Episode VIII - The Last Jedi","STUDIO","releaseYEAR", "LENGTH", "RATING", "DESC");
+		posterFade("<img src='img/poster_013.jpg'</img>");
+	});
+	$('#p14').on('click', function() {
+		movieName("Star Wars: The Force Awakens","STUDIO","releaseYEAR", "LENGTH", "RATING", "DESC");
+		posterFade("<img src='img/poster_014.jpg'</img>");
+	});
+	$('#p15').on('click', function() {
+		movieName("Star Wars: Return of the Jedi","STUDIO","releaseYEAR", "LENGTH", "RATING", "DESC");
+		posterFade("<img src='img/poster_015.jpg'</img>");
+	});
 	$('#sw-trailer-btn').on('click', function() {
 		document.getElementById("bannerImageID").style.display = "none";
 		document.getElementById("videoPlay").innerHTML = "<video id='sw-trailer'  width='100%' height='auto' autoplay controls> <source src='vid/vid.mp4' type='video/mp4'> Your browser does not support the video tag. </video>";
 		var video = document.getElementById('sw-trailer');
 		video.volume = 0.5;
 	});
+	var wrapper = document.getElementById('bookingWrapper');
+	var form = document.getElementById('form');
+	var theatre = {
+		numberOfSeats: 50
+	}
+	function createSeats(){
+		for (var i = 1; i < theatre.numberOfSeats+1; i++){
+			var element = document.createElement('div');
+			element.id = "seat_"+i;
+			element.className= 'available';
+			element.innerHTML = i;
+			element.addEventListener('click', showForm);
+			wrapper.appendChild(element);
+		}
+	};
+	createSeats();
+	function showForm(id){
+		var picked = document.getElementById('picked_seat');
+		form.style.display = "block";
+		var currentSeat = document.getElementById('current_seat');
+		currentSeat.value = this.id;
+		picked.innerText = "You have chosen to reserve seat " + this.id.substr(5,6);
+	};
+	var formButton = document.getElementById('formButton');
+	formButton.addEventListener('click', getFormData);
+	function getFormData(formName, formEmail, formSeat) {
+		var formName = document.getElementById('form_name');
+		var formEmail = document.getElementById('form_email');
+		var formSeat = document.getElementById('current_seat');
+		var reservationName = formName.value;
+		var reservationEmail = formEmail.value;
+		var reservationSeat = formSeat.value;
+		document.getElementById(reservationSeat).className="unavailable";
+		appendFormData(reservationName, reservationEmail, reservationSeat);
+		formName.value = "";
+		formEmail.value = "";
+	};
+	function appendFormData(name,email,seat){
+		form.style.display = "none";
+		var appendSeat = document.getElementById(seat);
+		var newElement = document.createElement('div');
+		newElement.className = "reservation";
+		document.getElementById(seat).appendChild(newElement);
+		newElement.innerHTML= '<span class="smallTxt">Reserved for</span><br><h4>' + name + '</h4>';
+	};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+### Animation: Project
+
+Oulu University of Applied Sciences
+<br /> Joona Keskitalo, DIN16SP, [t6kejo00@students.oamk.fi](mailto:t6kejo00@students.oamk.fi)
